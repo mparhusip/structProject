404306264375ae3da396d20759aa33466a7c1888
Marilyn Martha Yusnita Devi Parhusip <marilyn.parhusip@gmail.com>
2019-05-15 13:21:09 +0700

    Initial Commit

---
+++ structProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  structProject
+//
+//  Created by Marilyn Martha Yusnita Devi Parhusip on 15/05/19.
+//  Copyright © 2019 Apple Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
